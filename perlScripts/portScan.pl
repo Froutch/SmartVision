e957142a1980691465f9b0a426b62f613b2b4259
root <root@creakid.itinet.fr>
2013-05-13 09:54:54 +0200

    portScan.pl

--- perlScripts/portScan.pl
+++ perlScripts/portScan.pl
@@ -4,15 +4,31 @@ use strict;
 use warnings;
 my @arp_scan_line;
 my @arp_scan_line2;
+my @arp_scan_ip;
+my @arp_scan_mac;
+my @arp_scan_const;
 my $arp_scan = `arp-scan -l`;
 my $i = 0;
 my $j;
 my @tmp;
 
 @arp_scan_line = split("\n", $arp_scan);
+shift(@arp_scan_line);
+shift(@arp_scan_line);
+pop(@arp_scan_line);
+pop(@arp_scan_line);
+pop(@arp_scan_line);
 
 foreach $j (@arp_scan_line) 
 {
-	print $j;
-	print "\n";
+	@arp_scan_line2 = split("\t", $j);
+	$arp_scan_ip[$i] = $arp_scan_line2[0];
+	$arp_scan_mac[$i] = $arp_scan_line2[1];
+	$arp_scan_const[$i] = $arp_scan_line2[2];
+	print $arp_scan_ip[$i];
+	print "\t";
+	print $arp_scan_line2[$i];
+	print "\t";
+	print $arp_scan_line2[$i];
+	$i++;
 }

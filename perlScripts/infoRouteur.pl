eb34ec6256ec50d8cbffb101d3ffbfd0623155ab
root <root@creakid.itinet.fr>
2013-05-21 13:24:36 +0200

    change commande

--- perlScripts/infoRouteur.pl
+++ perlScripts/infoRouteur.pl
@@ -2,8 +2,11 @@
 
 use strict;
 
-my $item = "hrStorageSize.1";
-
-my $request = `snmpget -v 1 -c public 10.8.97.1 hrStorageSize.1`;
-my @request = split (/ +/, $request);
-my $result = $request[3];
+my @items = ("hrStorageSize.1");
+foreach my $item (@items)
+{
+	my $request = `snmpget -v 1 -c smartvision 10.8.97.1 $item`;
+	my @request = split (/ +/, $request);
+	my $result = $request[3];
+	print $result;
+}

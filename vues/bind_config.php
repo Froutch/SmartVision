742a1edc37ed262710026130f63840a35297d70c
Salman ALAMDAR <root@creakid.itinet.fr>
2013-05-22 19:48:01 +0200

    Modification de la page de config toujours

--- vues/bind_config.php
+++ vues/bind_config.php
@@ -8,28 +8,15 @@
 		<title>
 			Squelette
 		</title>
-		
-		<script> 
-
-//		var myselect = document.getElementById("selectOpt");
-	
-	function change_file() {
-		alert("lol");
-		//var newFile=myselect.value;
-		//var originalUrl=location.protocol + '//' + location.host + location.pathname;
-		//document.location.href=originalUrl+'?page=bind_config&f='+newFile;
-	}
-	
-</script>
 	</head>
 	<body>
 
 	<?php  ?>
 	<h1>Page de configuration de Bind</h1>
-	<br><br>
-<<<<<<< HEAD
-	<h3>/etc/bind/</h3>
+	<br>
+	
   <?php
+	
 	
 	if( !isset($_GET['f']) || !$_GET['f'] || strpos('/', $_GET['f']) || strpos('..', $_GET['f']) )
 	{
@@ -37,16 +24,22 @@
 	} else {
 		$filename = $_GET['f'];
 	}
-  
-	system("ls /etc/bind > ls_etc_bind.txt");
-	$handle = @fopen("/var/www/salman/vues/ls_etc_bind.txt", "r");
+	if (isset($_POST['script_modify'])) 
+	{
+		system("sudo \"".$_POST['script_modify']."\" > ".$filename."");
+	}
+	echo "<h3>/etc/bind/".$filename."</h3>";
+	system("sudo ls /etc/bind > ls_etc_bind.txt");
+	$handle = @fopen("/var/www/alex/vues/ls_etc_bind.txt", "r");
 	if ($handle) 
 	{
-		echo "<select onChange=\"change_file()\" id=\"selectOpt\">";
-		while (($buffer = trim(fgets($handle, 4096))) !== false) 
+		echo "<select onChange='change_file()' id='selectOpt'>";
+		while (($buffer = fgets($handle, 4096)) !== false) 
 		{
- 			echo "<option value=\"".$buffer."\" ";
-			if( $buffer == $filename ) echo "selected=\"selected\"";
+			$buffer = substr($buffer,0,-1);
+ 			echo "<option value='".$buffer."'";
+			
+			if($buffer == $filename) echo " selected='selected' ";
 			echo ">".$buffer."</option>";
  		}
  		echo"</select>";
@@ -57,34 +50,21 @@
 		fclose($handle);
 	}		
 ?>
-
+<script> 
+	var myselect = document.getElementById("selectOpt");
 	
-=======
-	<h3>/etc/bind/
-		  <?php
-                system("ls /etc/bind > ls_etc_bind.txt");
-                $handle = @fopen("/var/www/alex/vues/ls_etc_bind.txt", "r");
-                        if ($handle) {
-				        echo "<select>";
-    					while (($buffer = fgets($handle, 4096)) !== false) {
-       					 echo "<option>".$buffer."</option>";
-   				 }
-      				  echo"</select>";
-   				 if (!feof($handle)) {
-        				echo "Erreur: fgets() a échoué\n";
-
-					}
-   				 fclose($handle);
-				}		
-			?>
-
-	</h3>
->>>>>>> 404083d510f777af54d1a5afa0e79f962128c462
-	<form method="post" action="bind_config.php">
-		<textarea name="precisions" id="precisions" cols="70" rows="30">
-			<?php echo system("cat /etc/bind/".$filename.""); ?>
-	</textarea>
-	</form>
+	function change_file() {
+		var newFile=myselect.value;
+		var originalUrl=location.protocol + '//' + location.host + location.pathname;
+		document.location.href=originalUrl+'?page=bind_config&f='+newFile;
+	}
+	
+</script>
+	
+	<?php echo'<form method="post" action="index.php?page=bind_config&f='.$filename.'">'; ?>
+		<textarea name="script_modify" cols="70" rows="30"><?php echo system("cat /etc/bind/".$filename.""); ?></textarea>
+	
+	<input type='submit' value='Modifier' /> </form>
 	<br><hr><br>
 
 	</form>

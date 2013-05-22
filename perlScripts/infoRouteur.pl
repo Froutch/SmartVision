73851d3be1f0bde8a11847d30e698a67951c1982
titoupet <root@creakid.itinet.fr>
2013-05-22 15:12:09 +0200

    dans tableau

--- perlScripts/infoRouteur.pl
+++ perlScripts/infoRouteur.pl
@@ -2,7 +2,9 @@
 
 use strict;
 
-my @items = ("ifSpeed.1", "ifInOctets.2", "ifOutOctets.2","ifInErrors.1");
+#recuperation du taux de transmission, debits, et paquets rejetes
+
+my @items = ("ifSpeed.1", "ifInOctets.2", "ifOutOctets.2","ifInErrors.2");
 foreach my $item (@items)
 {
 	my $request = `snmpget -v 1 -c smartvision 10.8.97.1 $item`;

cac59b87bc37fccf7a75260316ed63119061fae8
Salman ALAMDAR <root@creakid.itinet.fr>
2013-05-22 14:31:32 +0200

    Modification supplémentaire

--- vues/bind_config.php
+++ vues/bind_config.php
@@ -8,34 +8,63 @@
 		<title>
 			Squelette
 		</title>
+		
+		<script> 
+
+//		var myselect = document.getElementById("selectOpt");
+	
+	function change_file() {
+		alert("lol");
+		//var newFile=myselect.value;
+		//var originalUrl=location.protocol + '//' + location.host + location.pathname;
+		//document.location.href=originalUrl+'?page=bind_config&f='+newFile;
+	}
+	
+</script>
 	</head>
 	<body>
+
 	<?php  ?>
 	<h1>Page de configuration de Bind</h1>
 	<br><br>
-	<h3>/etc/bind/named.conf.local</h3>
+	<h3>/etc/bind/</h3>
+  <?php
+	
+	if( !isset($_GET['f']) || !$_GET['f'] || strpos('/', $_GET['f']) || strpos('..', $_GET['f']) )
+	{
+		$filename = 'named.conf.local';
+	} else {
+		$filename = $_GET['f'];
+	}
+  
+	system("ls /etc/bind > ls_etc_bind.txt");
+	$handle = @fopen("/var/www/salman/vues/ls_etc_bind.txt", "r");
+	if ($handle) 
+	{
+		echo "<select onChange=\"change_file()\" id=\"selectOpt\">";
+		while (($buffer = trim(fgets($handle, 4096))) !== false) 
+		{
+ 			echo "<option value=\"".$buffer."\" ";
+			if( $buffer == $filename ) echo "selected=\"selected\"";
+			echo ">".$buffer."</option>";
+ 		}
+ 		echo"</select>";
+		if (!feof($handle)) 
+		{
+			echo "Erreur: fgets() a échoué\n";
+		}
+		fclose($handle);
+	}		
+?>
+
+	
 	<form method="post" action="bind_config.php">
 		<textarea name="precisions" id="precisions" cols="70" rows="30">
-			<?php echo system("cat /etc/bind/named.conf.local"); ?>
+			<?php echo system("cat /etc/bind/".$filename.""); ?>
 	</textarea>
 	</form>
 	<br><hr><br>
-		
-		<?php
-		system("ls /etc/bind > ls_etc_bind.txt");
-		$handle = @fopen("/var/www/alex/vues/texte.txt", "r");
-			if ($handle) {
-	echo "<select>";
-    while (($buffer = fgets($handle, 4096)) !== false) {
-        echo "<option>".$buffer."</option>";
-    }
-	echo"</select>";
-    if (!feof($handle)) {
-        echo "Erreur: fgets() a échoué\n";
-    }
-    fclose($handle);
-}
-?>
+
 	</form>
 	</body>
 	

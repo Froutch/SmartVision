cf9d9fe33cc361f6df518121c75c95b5f4d1e45d
Guillaume Roux <root@creakid.itinet.fr>
2013-05-30 09:38:12 +0200

    graph

--- perlScripts/bandwidth.pl
+++ perlScripts/bandwidth.pl
@@ -7,5 +7,15 @@ my $traficOut = `snmpget -v 1 -c public localhost .ifOutOctets.2`;
 my @traficI = split(/ +/, $traficIn);
 my @traficO = split(/ +/, $traficOut);
 
-print ("Octets In : $traficI[3]");
-print ("Octets Out : $traficO[3]");
+my $i=0;
+while ($i < 11)
+{
+	system `rrdtool update ./bandwidth.rrd N:$traficI[3]`;
+	system `rrdtool update ./bandwidth2.rrd N:$traficO[3]`;
+	$i++;
+	sleep 5;
+}
+system `rrdtool graph /var/www/guigui/graph/traffic_heure.png --start -3600 --vertical-label bps DEF:inoctets=/var/www/guigui/perlScripts/bandwidth.rrd:buffer:AVERAGE DEF:outoctets=/var/www/guigui/perlScripts/bandwidth2.rrd:buffer:AVERAGE CDEF:inbps=inoctets,8,\* CDEF:outbps=outoctets,8,\* AREA:inbps#00FF00:"Traffic entrant" LINE1:outbps#0000FF:"Traffic sortant"`;
+
+#print ("Octets In : $traficI[3]");
+#print ("Octets Out : $traficO[3]");

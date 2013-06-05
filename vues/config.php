ead190b17c92a653d71627be4a309e357a1da142
Michel GILLE <root@creakid.itinet.fr>
2013-06-05 23:44:34 +0200

    a

--- vues/config.php
+++ vues/config.php
@@ -3,14 +3,14 @@
 <?php
 if ($host == 'localhost')
 {
-	echo '<a href="index.php?page=terminal" data-role="button" data-inline="true">Terminal SSH</a>';
-	echo '<a href="index.php?page=cloud" data-theme="b" data-role="button" data-inline="true">Cloud</a>';
-	echo '<a href="index.php?page=scan" data-theme="d" data-role="button" data-inline="true">Scan Réseau</a>';
-
 	echo"<h1>Localhost</h1>";
 	echo"<h4>Machine installée le ";
 	system('sudo perlScripts/date_system.sh');
 	echo"</h4>";
+
+	echo '<a href="index.php?page=terminal" data-role="button" data-inline="true">Terminal SSH</a>';
+	echo '<a href="index.php?page=cloud" data-theme="b" data-role="button" data-inline="true">Cloud</a>';
+	echo '<a href="index.php?page=scan" data-theme="d" data-role="button" data-inline="true">Scan Réseau</a>';
 
 
 ?>

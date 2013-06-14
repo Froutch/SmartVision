933bc95080d4a00eaea145eb782413f9e9c59309
Salman ALAMDAR <root@creakid.itinet.fr>
2013-06-14 14:44:41 +0200

    PLusieurs modification et ajout d'interface

---
+++ explorer/config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array("admin","0639c1f0b0705bb4d222f9328cdd8d1c","/var/www","http://localhost",1,"",7,1),
+); 
+?>

517ab49cebfb9b9a684d3d827d14bde2d9139dfe
Salman ALAMDAR <root@creakid.itinet.fr>
2013-06-20 22:14:51 +0200

    Modification

--- edit_config.php
+++ edit_config.php
@@ -1,17 +1,23 @@
+	<body>
 	<center><h1>SmartVision file editing tool</h1></center>
 	<br>
 	
 <?php
+var_dump($_POST);
+var_dump($_GET);
+
 	if( !isset($_GET['f']) || !$_GET['f'] || strpos('/', $_GET['f']) || strpos('..', $_GET['f']) ) { }
 	else $filename = $_GET['f'];
 	
 	if( !isset($_POST['f']) || !$_POST['f'] || strpos('/', $_POST['f']) || strpos('..', $_POST['f']) ) { }
 	else $filename = $_POST['f'];
+	
+
 ?>
 <h3> Please enter here a file name </h3>
 
 	<?php echo'<form method="POST" action="index.php?page=edit_config">'; ?>
-	<input type="text" name="f" value="<?php if(isset($_POST['f'])) echo $_POST['f']; if(isset($_GET['f'])) echo $_GET['f']; ?>"required> <br>
+	<input type="text" name="f" required> <br>
 	
 <fieldset>
 	<label for="choices">
@@ -23,10 +29,14 @@
 			</li>
 		</ul>
 	</label>
+
 </fieldset>
+
 	<input type="submit" value="Open file" />
 </form>
 
+
+	
 <?php
 	
 	if (isset($_POST['script_modify'])) 
@@ -70,10 +80,12 @@
 		document.location.href=originalUrl+'?page=edit_config&f='+newFile;
 	}
 </script>
+	
 	<?php echo'<form method="post" action="index.php?page=edit_config&f='.$filename.'">'; ?>
 		<textarea name="script_modify" cols="70" rows="30"><?php $contenu = fread(fopen($filename, "r"), filesize($filename)); print $contenu; ?></textarea>
 		<input type='submit' value='Apply' />
 	</form>
 
 	</body>
+
 </html>
